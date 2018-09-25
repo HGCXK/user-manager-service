4f4fd239516d62f935975c57010e4926d07d9c4a
HG <HG@192.168.152.134>
2018-09-25 14:15:48 +0800

    用户service端可以集成eureka

--- src/main/java/com/ql/hg/UserManagerApplication.java
+++ src/main/java/com/ql/hg/UserManagerApplication.java
@@ -2,8 +2,10 @@ package com.ql.hg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserManagerApplication {
 
 	public static void main(String[] args) {
