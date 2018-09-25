bf06a36e5093e07e9bbbb317b7081c14876794e9
HG <HG@192.168.152.134>
2018-09-25 15:24:44 +0800

    添加spring-data-redis/spring-session

--- src/main/java/com/ql/hg/user/controller/MenuController.java
+++ src/main/java/com/ql/hg/user/controller/MenuController.java
@@ -2,15 +2,12 @@ package com.ql.hg.user.controller;
 
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ql.hg.user.entity.Menu;
@@ -27,6 +24,7 @@ import com.ql.hg.utils.PageUtils;
  */
 @Controller
 @RequestMapping("/menu")
+@EnableRedisHttpSession
 public class MenuController {
 	
 	@Autowired
